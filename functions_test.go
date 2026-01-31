a4614c476fa6d0a1ec5ca0eca617dd0884eb8481
Caio Cotts <caio@cotts.com.br>
2026-01-31 12:42:28 -0500

    add tests for calculator functions

---
+++ functions_test.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Functions", func() {
+	var stack Stack[string]
+
+	BeforeEach(func() {
+		stack = Stack[string]{}
+	})
+
+	Describe("Add", func() {
+		DescribeTable("Adding values together", func(a string, b string, expected string) {
+			stack.Push(a)
+			stack.Push(b)
+			_ = Add(&stack)
+
+			result, _ := stack.Pop()
+
+			Expect(result).To(Equal(expected))
+		},
+			Entry("should be able to add whole numbers", "-4", "3", "-1"),
+			Entry("should be able to add mixed float and whole numbers", "1", "1.5", "2.5"),
+			Entry("should be able to add floating point numbers", "1.5", "1.5", "3"),
+		)
+		It("should return an error on an incorrect number of arguments", func() {
+			err := Add(&stack)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+
+	Describe("Subtract", func() {
+		DescribeTable("Subtracting values", func(a string, b string, expected string) {
+			stack.Push(a)
+			stack.Push(b)
+			_ = Subtract(&stack)
+
+			result, _ := stack.Pop()
+
+			Expect(result).To(Equal(expected))
+		},
+			Entry("should be able to subtract whole numbers", "4", "3", "1"),
+			Entry("should be able to subtract mixed float and whole numbers", "1", "1.5", "-0.5"),
+			Entry("should be able to subtract two floating point numbers", "1.5", "1.5", "0"),
+		)
+		It("should return an error on an incorrect number of arguments", func() {
+			err := Subtract(&stack)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+
+	Describe("Multiply", func() {
+		DescribeTable("Multiplying values", func(a string, b string, expected string) {
+			stack.Push(a)
+			stack.Push(b)
+			_ = Multiply(&stack)
+
+			result, _ := stack.Pop()
+
+			Expect(result).To(Equal(expected))
+		},
+			Entry("should be able to multiply whole numbers", "4", "3", "12"),
+			Entry("should be able to multiply mixed float and whole numbers", "-1", "1.5", "-1.5"),
+			Entry("should be able to multiply two floating point numbers", "1.5", "1.5", "2.25"),
+		)
+
+		It("should return an error on an incorrect number of arguments", func() {
+			err := Multiply(&stack)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+
+	Describe("Divide", func() {
+		DescribeTable("Divide values", func(a string, b string, expected string) {
+			stack.Push(a)
+			stack.Push(b)
+			_ = Divide(&stack)
+
+			result, _ := stack.Pop()
+
+			Expect(result).To(Equal(expected))
+		},
+			Entry("should be able to divide whole numbers", "4", "3", "1.3333333333333333"),
+			Entry("should be able to divide mixed float and whole numbers", "1", "1.5", "0.6666666666666666"),
+			Entry("should be able to divide two floating point numbers", "-1.5", "1.5", "-1"),
+		)
+
+		It("should return an error on an incorrect number of arguments", func() {
+			err := Divide(&stack)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+
+	Describe("Drop", func() {
+		It("should pop the stack once", func() {
+			stack.Push("3")
+			stack.Push("4")
+			stack.Push("5")
+
+			_ = Drop(&stack)
+
+			numbers := stack.ToSlice()
+			expected := []string{
+				"3",
+				"4",
+			}
+
+			Expect(numbers).To(Equal(expected))
+		})
+
+		It("should return an error on an incorrect number of arguments", func() {
+			err := Drop(&stack)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+
+	Describe("Dup", func() {
+		It("should duplicate the item at the first level of the stack once", func() {
+			stack.Push("3")
+			stack.Push("4")
+			stack.Push("5")
+
+			_ = Dup(&stack)
+
+			numbers := stack.ToSlice()
+			expected := []string{
+				"3",
+				"4",
+				"5",
+				"5",
+			}
+
+			Expect(numbers).To(Equal(expected))
+		})
+
+		It("should return an error on an incorrect number of arguments", func() {
+			err := Dup(&stack)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+
+	Describe("Clear", func() {
+		It("should remove all values from the stack", func() {
+			stack.Push("3")
+			stack.Push("4")
+			stack.Push("5")
+
+			_ = Clear(&stack)
+
+			numbers := stack.ToSlice()
+			var expected []string
+
+			Expect(numbers).To(Equal(expected))
+		})
+	})
+
+	Describe("Swap", func() {
+		It("should swap the values from level 1 and 2 of the stack", func() {
+			stack.Push("3")
+			stack.Push("4")
+			stack.Push("5")
+
+			_ = Swap(&stack)
+
+			numbers := stack.ToSlice()
+			expected := []string{
+				"3",
+				"5",
+				"4",
+			}
+
+			Expect(numbers).To(Equal(expected))
+		})
+
+		It("should return an error on an incorrect number of arguments", func() {
+			err := Divide(&stack)
+			Expect(err).To(Not(BeNil()))
+		})
+	})
+})
--- go.mod
+++ go.mod
@@ -2,13 +2,27 @@ module github.com/caiocotts/rpnc
 
 go 1.25
 
-require github.com/gdamore/tcell/v2 v2.13.2
+require (
+	github.com/gdamore/tcell/v2 v2.13.2
+	github.com/onsi/ginkgo/v2 v2.27.5
+	github.com/onsi/gomega v1.39.0
+)
 
 require (
+	github.com/Masterminds/semver/v3 v3.4.0 // indirect
 	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/term v0.37.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
+	golang.org/x/tools v0.38.0 // indirect
 )
